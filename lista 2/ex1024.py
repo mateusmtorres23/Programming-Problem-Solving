e93926422f5c26d8df86dde84bcdf32e38ccb686
mateusmtorres23 <mmtorres230726@gmail.com>
2025-06-02 23:52:19 -0300

    Update ex1024.py

--- lista 2/ex1024.py
+++ lista 2/ex1024.py
@@ -1,15 +1,13 @@
-tests = int(input())
+for _ in range(int(input())):
+    og = input()
+    mod = ''
+    for let in og:
+        mod += chr(ord(let)+3) if let.isalpha() else let
 
-for i in range(tests):
-    text = input()
-    mod_text = ''
-    for letter in text:
-        mod_text += chr(ord(letter)+3) if letter.isalpha() else letter
-    
-    inv_text = mod_text[::-1]
-    half = len(inv_text) //2
-    fin_text = inv_text[:half]
-    for letter in inv_text[half:]:
-        fin_text += chr(ord(letter)-1)
-
-    print(fin_text)
+    inv = mod[::-1]
+    half = len(inv)//2
+    final = inv[:half]
+    for let in inv[half:]:
+        sub = chr(ord(let)-1)
+        final += sub
+    print(final)
