e12c0aa18d627aa5a422edc2ad40428488c6bb6a
mateusmtorres23 <mmtorres230726@gmail.com>
2025-06-02 21:09:34 -0300

    Update ex1018.py

--- lista 1/ex1018.py
+++ lista 1/ex1018.py
@@ -1,8 +1,5 @@
-money = int(input())
-print(money)
-bills = [(100, 'R$ 100,00'), (50, 'R$ 50,00'), (20, 'R$ 20,00'), 
-         (10, 'R$ 10,00'), (5, 'R$ 5,00'), (2, 'R$ 2,00'), (1, 'R$ 1,00')]
-for i in bills: 
-    num_bills=money//i[0]
-    money-=i[0]*num_bills
-    print(f'{num_bills} nota(s) de {i[1]}')
+num = int(input());print(num)
+values = [(100,'100,00'),(50,'50,00'),(20,'20,00'),(10,'10,00'),(5,'5,00'),(2,'2,00'),(1,'1,00')]
+for div, note in values:
+    print(f'{num//div} nota(s) de R$ {note}')
+    num-= (num//div)*div
