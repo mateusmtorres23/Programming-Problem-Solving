282677fec17130443adc13e8905908a9e0a83e8d
mateusmtorres23 <mmtorres230726@gmail.com>
2025-07-16 17:29:05 -0300

    Delete lista 1 directory

--- lista 1/ex1018.py
+++
@@ -1,5 +0,0 @@
-num = int(input());print(num)
-values = [(100,'100,00'),(50,'50,00'),(20,'20,00'),(10,'10,00'),(5,'5,00'),(2,'2,00'),(1,'1,00')]
-for div, note in values:
-    print(f'{num//div} nota(s) de R$ {note}')
-    num-= (num//div)*div
--- lista 1/ex1020.py
+++
@@ -1,4 +0,0 @@
-age = int(input())
-year = age//365; age-=year*365
-month = age//30; age-=month*30
-print(f'{year} ano(s)\n{month} mes(es)\n{age} dia(s)')
